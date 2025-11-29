3b70faac94d42deec40060e1ff199f2afd154ae3
Kim Costello <kimberlyabarfield@gmail.com>
2025-11-28 23:22:20 -0500

    created API file

---
+++ final_api/plumber.R
@@ -0,0 +1,108 @@
+# Find out more about building APIs with Plumber here:
+#
+#    https://www.rplumber.io/
+#
+
+# Read in data and fit best model to the entire data set
+library(readr)
+library(dplyr)
+library(janitor)
+library(ranger)
+
+diabetes_data <- read_csv("../diabetes_binary_health_indicators_BRFSS2015.csv")
+
+diabetes_data <- diabetes_data |>
+  mutate(
+    Diabetes_binary = factor(Diabetes_binary, 
+                             levels = c(0, 1),
+                             labels = c("No diabetes",
+                                        "Prediabetes/Diabetes")),
+    HighBP = factor(HighBP,
+                    levels = c(0, 1),
+                    labels = c("Not high BP", "High BP")),
+    HighChol = factor(HighChol,
+                      levels = c(0, 1),
+                      labels = c("Not high cholesterol", "High cholesterol")), 
+    Smoker = factor(Smoker,
+                    levels = c(0, 1),
+                    labels = c("Non-smoker", "Smoker")),
+    Stroke = factor(Stroke,
+                    levels = c(0, 1),
+                    labels = c("No stroke", "Stroke")),
+    HeartDiseaseorAttack = factor(HeartDiseaseorAttack,
+                                  levels = c(0, 1),
+                                  labels = c("No heart disease/attack", 
+                                             "Heart disease/attack")),
+    PhysActivity = factor(PhysActivity,
+                          levels = c(0, 1),
+                          labels = c("No physical activity", "Physical activity")),
+    HvyAlcoholConsump = factor(HvyAlcoholConsump,
+                               levels = c(0, 1),
+                               labels = c("Not a heavy drinker", "Heavy drinker")),
+    AnyHealthcare = factor(AnyHealthcare,
+                           levels = c(0, 1),
+                           labels = c("No healthcare", "Healthcare")),
+    GenHlth = factor(GenHlth,
+                     levels = c(1, 2, 3, 4, 5),
+                     labels = c("Excellent", "Very good", 
+                                "Good", "Fair", "Poor")),
+    Sex = factor(Sex,
+                 levels = c(0, 1),
+                 labels = c("Female", "Male")),
+    Age = factor(Age,
+                 levels = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13),
+                 labels = c("18-24", "25-29", "30-34", "35-39", "40-44",
+                            "45-49", "50-54", "55-59", "60-64", "65-69",
+                            "70-74", "75-79", "80 or older"))
+  ) |>
+  select(-CholCheck, -NoDocbcCost, -Fruits, -Veggies, -Income, -Education, 
+         -DiffWalk) |>
+  clean_names("snake")
+
+diabetes_recipe <- recipe(diabetes_binary ~ ., data = diabetes_train) |>
+  update_role(age, new_role = "ID") |>
+  step_rm(phys_hlth, ment_hlth, any_healthcare) |>
+  step_dummy(high_bp, high_chol, smoker, stroke, phys_activity,
+             heart_diseaseor_attack, hvy_alcohol_consump, sex, gen_hlth) |>
+  step_normalize(bmi)
+
+forest_model <- rand_forest(mtry = 4, trees = 100) |>
+  set_engine("ranger") |>
+  set_mode("classification")
+
+forest_workflow <- workflow() |>
+  add_recipe(diabetes_recipe) |>
+  add_model(forest_model)
+
+forest_fit <- forest_workflow |>
+  fit(diabetes_data)
+
+
+
+library(plumber)
+
+#* @apiTitle Diabetes Health Indicators API
+#* @apiDescription PUT DESCRIPTION HERE.
+
+#* Info endpoint
+#* @get /info
+function() {
+    list(Author = "Kim Costello", 
+         URL = "") 
+}
+
+#* Predictor variables
+#* @param var
+#* @get /pred
+function() {
+    
+}
+
+
+# Programmatically alter your API
+#* @plumber
+function(pr) {
+    pr %>%
+        # Overwrite the default serializer to return unboxed JSON
+        pr_set_serializer(serializer_unboxed_json())
+}
